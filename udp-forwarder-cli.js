93223404f601d90e512cba8682f9beaafcb6ce60
Devendra Tewari <devendra.tewari@gmail.com>
2018-08-16 12:26:27 -0300

    closes #2

--- udp-forwarder-cli.js
+++ udp-forwarder-cli.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
-const udpf = require('./udp-forwarder.js');
+var argv = require("commander");
+var packageConfig = require('./package.json');
 
-const argv = require("optimist")
-.usage("Usage: $0"
-+ " --destinationPort [port] --destinationAddress [address]"
-+ " [--protocol [udp4|udp6]]"
-+ " [--port [port]] [--address [address]]"
-+ " [--multicastAddress [address]]"
-+ " [--forwarderPort [port]] [--forwarderAddress [address]]")
-.demand(["destinationPort", "destinationAddress"])
-.boolean("q")
-.argv;
+argv
+.usage("[options]")
+.version(packageConfig.version)
+.option("-p, --port [number]", "Listen at specified port number", parseInt)
+.option("-a, --address [address]", "Listen at specified IP address")
+.option("-m, --multicastAddress [address]",
+    "Join and listen for datagrams from specified multicast group")
+.option("-i, --protocol [udp4|udp6]",
+    "Specify udp4 to use IPv4, udp6 to use IPv6", "udp4")
+.option("-d, --destinationPort <number>", "Forward to specified port", parseInt)
+.option("-n, --destinationAddress <host>",
+    "Forward to specified host name, or unicast or multicast IP address")
+.option("-f, --forwarderPort [number]", "Forward from specified port", parseInt)
+.option("-o, --forwarderAddress [address]", "Forward from specified IP address")
+.parse(process.argv);
 
 const options = {
     protocol: argv.protocol,
@@ -22,7 +28,8 @@ const options = {
     created: created
 };
 
-const f = udpf.create(argv.destinationPort, argv.destinationAddress, options);
+const f = require('./udp-forwarder.js').create(argv.destinationPort,
+    argv.destinationAddress, options);
 
 function created() {
     console.log(`listening on ${f.address}:${f.port}`);
